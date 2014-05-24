19ed9d43d58c37fcbb412f7e5ecb09a2b0c7fdf7
Manaenkov Grigoriy manaenkov gregory <Manaenkov Grigoriy manaenkov.gregory@gmail.com>
2014-05-24 19:38:25 +0300

    Initialisation
     - ????? ??? ??????? ???? ? ????? ? ????? ????????????????

---
+++ DartsConsole/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartsConsole
+{
+    class Team
+    {
+        public int Id { get; set; }
+        [StringLength(200)]        
+        public string Name { get; set; }
+    }
+}
