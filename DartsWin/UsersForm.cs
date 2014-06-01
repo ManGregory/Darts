abc1f8698c32df4104f61082f94f386b4b932ac6
Manaenkov Grigoriy manaenkov gregory <ManGregory@users.noreply.github.com>
2014-06-01 20:59:01 +0300

    Begin developing form for users catalog
     - ?????? ?????????? ????? ??? ?????? ? ????????

---
+++ DartsWin/UsersForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DartsConsole;
+
+namespace DartsWin
+{
+    public partial class UsersForm : Telerik.WinControls.UI.RadForm
+    {
+        private Db _connectionDb;
+
+        public UsersForm(Db connectionDb)
+        {
+            InitializeComponent();
+            _connectionDb = connectionDb;
+            var users = _connectionDb.ConnectionContext.Users.ToList();
+            gridUsers.DataSource = users;
+            // todo set columns width and other attrs
+            gridUsers.Columns["Id"].IsVisible = false;
+            gridUsers.Columns["TeamsAttending"].IsVisible = false;
+        }
+
+        private void gridUsers_UserAddedRow(object sender, Telerik.WinControls.UI.GridViewRowEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // todo saving changes in db            
+        }
+    }
+}
