03624fe652da9a42093139138573acfa269db81a
Manaenkov Grigoriy manaenkov gregory <Manaenkov Grigoriy manaenkov.gregory@gmail.com>
2014-06-01 20:57:58 +0300

    Create class for db connection in gui project
     - ???????? ????? ??????????? ? ???? ??? ??????????
     - ???????? ???????? ???? ?????? ? ????????

--- DartsWin/Program.cs
+++ DartsWin/Program.cs
@@ -15,7 +15,7 @@ namespace DartsWin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
