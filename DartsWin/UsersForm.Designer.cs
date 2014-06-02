510cc65b18f748e5115e495bc25309be7013b4e5
Manaenkov Grigoriy manaenkov gregory <ManGregory@users.noreply.github.com>
2014-06-02 21:22:05 +0300

    Users form - customise grid view, saving changes in the grid
     - ?????????? ??????? - ??? ???????, ?????????? ?????????

--- DartsWin/UsersForm.Designer.cs
+++ DartsWin/UsersForm.Designer.cs
@@ -28,91 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnlTop = new Telerik.WinControls.UI.RadPanel();
-            this.btnAdd = new Telerik.WinControls.UI.RadButton();
             this.pnlData = new Telerik.WinControls.UI.RadPanel();
             this.gridUsers = new Telerik.WinControls.UI.RadGridView();
-            this.pnlBottom = new Telerik.WinControls.UI.RadPanel();
-            ((System.ComponentModel.ISupportInitialize)(this.pnlTop)).BeginInit();
-            this.pnlTop.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.btnAdd)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pnlData)).BeginInit();
             this.pnlData.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridUsers)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridUsers.MasterTemplate)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pnlBottom)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pnlTop
-            // 
-            this.pnlTop.Controls.Add(this.btnAdd);
-            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlTop.Location = new System.Drawing.Point(0, 0);
-            this.pnlTop.Name = "pnlTop";
-            this.pnlTop.Size = new System.Drawing.Size(298, 44);
-            this.pnlTop.TabIndex = 0;
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnAdd.Location = new System.Drawing.Point(0, 0);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(92, 44);
-            this.btnAdd.TabIndex = 1;
-            this.btnAdd.Text = "Добавить";
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // pnlData
             // 
             this.pnlData.Controls.Add(this.gridUsers);
             this.pnlData.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlData.Location = new System.Drawing.Point(0, 44);
+            this.pnlData.Location = new System.Drawing.Point(0, 0);
             this.pnlData.Name = "pnlData";
-            this.pnlData.Size = new System.Drawing.Size(298, 188);
+            this.pnlData.Size = new System.Drawing.Size(528, 279);
             this.pnlData.TabIndex = 1;
             // 
             // gridUsers
             // 
+            this.gridUsers.BeginEditMode = Telerik.WinControls.RadGridViewBeginEditMode.BeginEditOnEnter;
             this.gridUsers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridUsers.EnableCustomFiltering = true;
             this.gridUsers.Location = new System.Drawing.Point(0, 0);
+            // 
+            // gridUsers
+            // 
+            this.gridUsers.MasterTemplate.EnableCustomFiltering = true;
+            this.gridUsers.MasterTemplate.EnableFiltering = true;
             this.gridUsers.Name = "gridUsers";
-            this.gridUsers.Size = new System.Drawing.Size(298, 188);
+            this.gridUsers.NewRowEnterKeyMode = Telerik.WinControls.UI.RadGridViewNewRowEnterKeyMode.EnterMovesToNextCell;
+            this.gridUsers.Size = new System.Drawing.Size(528, 279);
             this.gridUsers.TabIndex = 0;
-            this.gridUsers.Text = "radGridView1";
-            this.gridUsers.UserAddedRow += new Telerik.WinControls.UI.GridViewRowEventHandler(this.gridUsers_UserAddedRow);
-            // 
-            // pnlBottom
-            // 
-            this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlBottom.Location = new System.Drawing.Point(0, 232);
-            this.pnlBottom.Name = "pnlBottom";
-            this.pnlBottom.Size = new System.Drawing.Size(298, 47);
-            this.pnlBottom.TabIndex = 1;
+            this.gridUsers.CellEndEdit += new Telerik.WinControls.UI.GridViewCellEventHandler(this.gridUsers_CellEndEdit);
             // 
             // UsersForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(298, 279);
+            this.ClientSize = new System.Drawing.Size(528, 279);
             this.Controls.Add(this.pnlData);
-            this.Controls.Add(this.pnlBottom);
-            this.Controls.Add(this.pnlTop);
             this.Name = "UsersForm";
             // 
             // 
             // 
             this.RootElement.ApplyShapeToControl = true;
-            this.Text = "Users";
+            this.Text = "Игроки";
             this.ThemeName = "ControlDefault";
-            ((System.ComponentModel.ISupportInitialize)(this.pnlTop)).EndInit();
-            this.pnlTop.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.btnAdd)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pnlData)).EndInit();
             this.pnlData.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gridUsers.MasterTemplate)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridUsers)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pnlBottom)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
 
@@ -120,10 +87,7 @@
 
         #endregion
 
-        private Telerik.WinControls.UI.RadPanel pnlTop;
         private Telerik.WinControls.UI.RadPanel pnlData;
-        private Telerik.WinControls.UI.RadPanel pnlBottom;
-        private Telerik.WinControls.UI.RadButton btnAdd;
         private Telerik.WinControls.UI.RadGridView gridUsers;
     }
 }
