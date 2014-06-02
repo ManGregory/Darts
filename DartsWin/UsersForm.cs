aef2f338bf5792dbedeaef799973825ab63e5370
Manaenkov Grigoriy manaenkov gregory <Manaenkov Grigoriy manaenkov.gregory@gmail.com>
2014-06-02 21:22:05 +0300

    Users form - customise grid view, saving changes in the grid
     - ?????????? ??????? - ??? ???????, ?????????? ?????????

--- DartsWin/UsersForm.cs
+++ DartsWin/UsersForm.cs
@@ -1,33 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using DartsConsole;
+using System.Data.Entity;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
 
 namespace DartsWin
 {
     public partial class UsersForm : Telerik.WinControls.UI.RadForm
     {
         private Db _connectionDb;
+        private BindingSource _usersBindingSource = new BindingSource();
 
         public UsersForm(Db connectionDb)
         {
             InitializeComponent();
             _connectionDb = connectionDb;
-            var users = _connectionDb.ConnectionContext.Users.ToList();
-            gridUsers.DataSource = users;
+            _connectionDb.ConnectionContext.Users.Load();
+            _usersBindingSource.DataSource = _connectionDb.ConnectionContext.Users.Local.ToBindingList();
+            gridUsers.DataSource = _usersBindingSource;
+            // todo add excel like filtering
+
+            // todo add localisation
+
+            gridUsers.MasterTemplate.AutoSizeColumnsMode = GridViewAutoSizeColumnsMode.Fill;
             // todo set columns width and other attrs
             gridUsers.Columns["Id"].IsVisible = false;
             gridUsers.Columns["TeamsAttending"].IsVisible = false;
+            gridUsers.Columns["TeamsAttending"].ReadOnly = true;
+            gridUsers.Columns["TeamsAttending"].HeaderText = "Команды";
+            gridUsers.Columns["TeamsAttending"].Width = 100;
+            gridUsers.Columns["Name"].Width = 200;
+            gridUsers.Columns["Name"].HeaderText = "Игрок";
+            gridUsers.Columns["Email"].Width = 300;            
         }
 
-        private void gridUsers_UserAddedRow(object sender, Telerik.WinControls.UI.GridViewRowEventArgs e)
+        private void gridUsers_CellEndEdit(object sender, Telerik.WinControls.UI.GridViewCellEventArgs e)
         {
-
-        }
-
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            // todo saving changes in db            
+            try
+            {
+                _connectionDb.ConnectionContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }            
         }
     }
 }
