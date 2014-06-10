af6143dff6755d06beba208728f037c2e292bf57
Manaenkov Grigoriy manaenkov gregory <Manaenkov Grigoriy manaenkov.gregory@gmail.com>
2014-06-10 22:27:24 +0300

    Add game form
     - ????????? ????? ??????? ????

---
+++ DartsWin/GameForms.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DartsWin
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpRule = new Telerik.WinControls.UI.RadGroupBox();
+            this.txtRuleDescription = new Telerik.WinControls.UI.RadTextBoxControl();
+            ((System.ComponentModel.ISupportInitialize)(this.grpRule)).BeginInit();
+            this.grpRule.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtRuleDescription)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpRule
+            // 
+            this.grpRule.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.grpRule.Controls.Add(this.txtRuleDescription);
+            this.grpRule.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grpRule.HeaderText = "Правила";
+            this.grpRule.Location = new System.Drawing.Point(0, 0);
+            this.grpRule.Name = "grpRule";
+            this.grpRule.Size = new System.Drawing.Size(646, 116);
+            this.grpRule.TabIndex = 0;
+            this.grpRule.Text = "Правила";
+            // 
+            // txtRuleDescription
+            // 
+            this.txtRuleDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtRuleDescription.Location = new System.Drawing.Point(2, 18);
+            this.txtRuleDescription.Multiline = true;
+            this.txtRuleDescription.Name = "txtRuleDescription";
+            this.txtRuleDescription.Size = new System.Drawing.Size(642, 96);
+            this.txtRuleDescription.TabIndex = 0;
+            this.txtRuleDescription.UseCompatibleTextRendering = false;
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 312);
+            this.Controls.Add(this.grpRule);
+            this.Name = "GameForm";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Игра";
+            this.ThemeName = "ControlDefault";
+            ((System.ComponentModel.ISupportInitialize)(this.grpRule)).EndInit();
+            this.grpRule.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.txtRuleDescription)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadGroupBox grpRule;
+        private Telerik.WinControls.UI.RadTextBoxControl txtRuleDescription;
+    }
+}
---
+++ DartsWin/GameForms.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using Telerik.WinControls;
+
+namespace DartsWin
+{
+    public partial class GameForm : Telerik.WinControls.UI.RadForm
+    {
+        private Db _connectionDb;
+        private DartsConsole.Rule _rule;
+        private BindingSource _membersBindingSource = new BindingSource();
+
+        public GameForm(Db connectionDb, DartsConsole.Rule rule, List<Member> members)
+        {
+            InitializeComponent();
+            _connectionDb = connectionDb;
+            _rule = rule;
+            if (rule.IsCommand)
+            {
+                _connectionDb.ConnectionContext.Teams.Load();
+                _membersBindingSource.DataSource = _connectionDb.ConnectionContext.Teams.Local.ToBindingList()
+                    .Where(m => members.Exists(m1 => m1.Id == m.Id));
+            }
+            else
+            {
+                _connectionDb.ConnectionContext.Users.Load();
+                _membersBindingSource.DataSource = _connectionDb.ConnectionContext.Users.Local.ToBindingList()
+                    .Where(m => members.Exists(m1 => m1.Id == m.Id));
+            }
+
+            Text = GetFormText();
+            txtRuleDescription.Text = _rule.Description;
+            txtRuleDescription.IsReadOnly = true;
+            txtRuleDescription.DeselectAll();
+        }
+
+        private string GetFormText()
+        {
+            return _rule.IsCommand
+                ? string.Format("Командная игра - {0}", _rule.Name)
+                : string.Format("Игра - {0}", _rule.Name);
+        }
+    }
+}
