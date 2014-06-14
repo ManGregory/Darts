a6ca80320f55e904cb5a3c4187594afcf0e0584f
Manaenkov Grigoriy manaenkov gregory <ManGregory@users.noreply.github.com>
2014-06-14 23:25:51 +0300

    Add interface elements for current player in game form
     - ????????? ???????? ?????????? ? ????? ????

--- DartsWin/GameForms.cs
+++ DartsWin/GameForms.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
 using DartsConsole;
 using Telerik.WinControls;
+using Telerik.WinControls.UI;
 
 namespace DartsWin
 {
@@ -42,6 +45,40 @@ namespace DartsWin
             txtRuleDescription.Text = _rule.Description;
             txtRuleDescription.IsReadOnly = true;
             txtRuleDescription.DeselectAll();
+        }
+
+        private IEnumerable<Control> EnumerateControls()
+        {
+            return from field in GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                   where typeof(Control).IsAssignableFrom(field.FieldType)
+                   let component = (Control)field.GetValue(this)
+                   where component != null
+                   select component;
+        }
+
+        private void ThrowEditValueChanged(object sender, EventArgs eventArgs)
+        {
+            var edit = (sender as NumericUpDown);
+            if (edit != null)
+            {
+                var lastNum = edit.Name.Substring(edit.Name.Length - 1);
+                var throwLabelSum = EnumerateControls().Single(e => e.Name == "lblSumThrow" + lastNum) as RadLabel;
+                var throwEdit = EnumerateControls().Single(e => e.Name == "edThrow" + lastNum) as NumericUpDown;
+                var throwFactor = EnumerateControls().Single(e => e.Name == "edFactor" + lastNum) as NumericUpDown;
+                if (throwLabelSum != null && throwEdit != null && throwFactor != null)
+                {
+                    throwLabelSum.Text = (throwEdit.Value*throwFactor.Value).ToString();
+                }
+            }
+            lblSumSerie.Text = GetSerieSum().ToString();
+        }
+
+        private decimal GetSerieSum()
+        {
+            return
+                (edThrow1.Value*edFactor1.Value) +
+                (edThrow2.Value*edFactor2.Value) +
+                (edThrow3.Value*edFactor3.Value);
         }
 
         private string GetFormText()
