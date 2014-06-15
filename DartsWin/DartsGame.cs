5df86f8966fbd6a8d790d8c013f4fac73a061d6f
Manaenkov Grigoriy manaenkov gregory <Manaenkov Grigoriy manaenkov.gregory@gmail.com>
2014-06-15 23:10:44 +0300

    Add classes to represent darts things
     - ????????? ?????? ??? ????????????? ?????? ??????? ?? ??????

---
+++ DartsWin/DartsGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartsWin
+{
+    abstract class DartsGame
+    {
+        public List<Member> _members;
+
+    }
+}
