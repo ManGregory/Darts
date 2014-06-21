590bbfa4c626f46c9a90aff8dda3d9d8622f26d3
Manaenkov Grigoriy manaenkov gregory <ManGregory@users.noreply.github.com>
2014-06-22 00:12:40 +0300

    Add grid player panel
     - ????????? ?????? ?????????? ? ??????? ??????? (??? ??????)

---
+++ DartsWin/GridPlayerPanel.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DartsWin
+{
+    partial class GridPlayerPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTeamName = new Telerik.WinControls.UI.RadPanel();
+            this.pnlTeamStatus = new Telerik.WinControls.UI.RadPanel();
+            this.pnlGrid = new Telerik.WinControls.UI.RadPanel();
+            this.gridTeam = new Telerik.WinControls.UI.RadGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlTeamName)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlTeamStatus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlGrid)).BeginInit();
+            this.pnlGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTeam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTeam.MasterTemplate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlTeamName
+            // 
+            this.pnlTeamName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTeamName.Location = new System.Drawing.Point(0, 0);
+            this.pnlTeamName.Name = "pnlTeamName";
+            this.pnlTeamName.Size = new System.Drawing.Size(195, 28);
+            this.pnlTeamName.TabIndex = 0;
+            this.pnlTeamName.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlTeamStatus
+            // 
+            this.pnlTeamStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlTeamStatus.Location = new System.Drawing.Point(0, 230);
+            this.pnlTeamStatus.Name = "pnlTeamStatus";
+            this.pnlTeamStatus.Size = new System.Drawing.Size(195, 28);
+            this.pnlTeamStatus.TabIndex = 1;
+            this.pnlTeamStatus.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlGrid
+            // 
+            this.pnlGrid.Controls.Add(this.gridTeam);
+            this.pnlGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGrid.Location = new System.Drawing.Point(0, 28);
+            this.pnlGrid.Name = "pnlGrid";
+            this.pnlGrid.Size = new System.Drawing.Size(195, 202);
+            this.pnlGrid.TabIndex = 2;
+            this.pnlGrid.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gridTeam
+            // 
+            this.gridTeam.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridTeam.Location = new System.Drawing.Point(0, 0);
+            // 
+            // gridTeam
+            // 
+            this.gridTeam.MasterTemplate.AllowAddNewRow = false;
+            this.gridTeam.MasterTemplate.AllowColumnReorder = false;
+            this.gridTeam.Name = "gridTeam";
+            this.gridTeam.ReadOnly = true;
+            this.gridTeam.Size = new System.Drawing.Size(195, 202);
+            this.gridTeam.TabIndex = 0;
+            // 
+            // GridPlayerPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlGrid);
+            this.Controls.Add(this.pnlTeamStatus);
+            this.Controls.Add(this.pnlTeamName);
+            this.Name = "GridPlayerPanel";
+            this.Size = new System.Drawing.Size(195, 258);
+            ((System.ComponentModel.ISupportInitialize)(this.pnlTeamName)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlTeamStatus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlGrid)).EndInit();
+            this.pnlGrid.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridTeam.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTeam)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadPanel pnlTeamName;
+        private Telerik.WinControls.UI.RadPanel pnlTeamStatus;
+        private Telerik.WinControls.UI.RadPanel pnlGrid;
+        private Telerik.WinControls.UI.RadGridView gridTeam;
+    }
+}
---
+++ DartsWin/GridPlayerPanel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
+using Telerik.WinControls.UI.Localization;
+
+namespace DartsWin
+{
+    public partial class GridPlayerPanel : UserControl
+    {
+        public GridPlayerPanel()
+        {
+            InitializeComponent();
+            gridTeam.EnableFiltering = true;
+            gridTeam.ShowFilteringRow = false;
+            gridTeam.AutoGenerateColumns = false;
+            gridTeam.FilterPopupRequired +=
+                (sender, args) => args.FilterPopup = new RadListFilterPopup(args.Column, true);
+            gridTeam.ShowGroupPanel = false;
+            gridTeam.AutoGenerateColumns = false;
+            gridTeam.AutoSizeColumnsMode = GridViewAutoSizeColumnsMode.Fill;
+            RadGridLocalizationProvider.CurrentProvider = new RussianRadGridLocalizationProvider();
+        }
+
+        public RadGridView TeamGrid
+        {
+            get
+            {
+                return gridTeam;
+            }
+        }
+
+        public RadPanel NamePanel
+        {
+            get
+            {
+                return pnlTeamName;
+            }
+        }
+
+        public RadPanel StatusPanel
+        {
+            get
+            {
+                return pnlTeamStatus;
+            }
+        }
+    }
+}
