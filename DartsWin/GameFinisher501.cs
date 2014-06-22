7d320190d5aa5e69ad62da022e10ddd213d13434
Manaenkov Grigoriy manaenkov gregory <Manaenkov Grigoriy manaenkov.gregory@gmail.com>
2014-06-22 23:07:33 +0300

    Create classes and interfaces for game finishing and checking bust
     - ????????? ?????? ? ?????????? ??? ???????? ????????? ???? ? ??????? ????????

---
+++ DartsWin/GameFinisher501.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartsWin
+{
+    // todo make abstract class for game with limit
+
+    class GameFinisher501 : IGameFinisher, IGameBuster
+    {
+        public bool IsGameFinished(int totalPoints, DartsSerie lastSerie)
+        {
+            return (
+                (totalPoints + lastSerie.GetSum() == 501) && 
+                (lastSerie.Throws.Last(t => t.GetSum() > 0).Score.IsDouble));
+        }
+
+        public bool IsGameBusted(int totalPoints, DartsSerie lastSerie)
+        {
+            return (
+                !IsGameFinished(totalPoints, lastSerie) && 
+                (totalPoints + lastSerie.GetSum() >= 500));
+        }
+    }
+}
