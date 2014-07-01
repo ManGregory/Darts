9fe83ef82d76f90d8f1f48ee843b810428ab8ba9
Manaenkov Grigoriy manaenkov gregory <Manaenkov Grigoriy manaenkov.gregory@gmail.com>
2014-07-01 22:15:33 +0300

    Refactoring for game checing finish and bust

---
+++ DartsLogic/GameChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartsLogic
+{
+    public abstract class GameChecker : IGameFinisher, IGameBuster
+    {
+        public virtual bool IsGameFinished(int totalPoints, DartsSerie lastSerie)
+        {
+            return false;
+        }
+
+        public virtual bool IsGameBusted(int totalPoints, DartsSerie lastSerie)
+        {
+            return false;
+        }
+    }
+}
---
+++ DartsLogic/GameCheckerFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DartsConsole;
+
+namespace DartsLogic
+{
+    public static class GameCheckerFactory
+    {
+        public static GameChecker GetGameChecker(Rule rule) 
+        {
+            if (rule.Name == "301") return new GameCheckerBust301();
+            return new GameCheckerBust501();
+        }
+    }
+}
