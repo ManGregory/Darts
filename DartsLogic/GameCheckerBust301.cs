09534f81c3abc87f263f5577dc1914406a9fb981
Manaenkov Grigoriy manaenkov gregory <ManGregory@users.noreply.github.com>
2014-07-01 22:15:33 +0300

    Refactoring for game checing finish and bust

---
+++ DartsLogic/GameCheckerBust301.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartsLogic
+{
+    public class GameCheckerBust301 : GameCheckerBust
+    {
+        public GameCheckerBust301()
+        {
+            Limit = 301;
+        }
+    }
+}
---
+++ DartsLogic/GameCheckerBust501.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace DartsLogic
+{
+    public class GameCheckerBust501 : GameCheckerBust
+    {
+        public GameCheckerBust501()
+        {
+            Limit = 501;
+        }
+    }
+}
